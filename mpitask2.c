6a17b9e5de3e4e908df764acc07a9b9a0fda53be
xhx0095 <71507775+xhx0095@users.noreply.github.com>
2021-02-12 21:19:14 +0800

    Update mpitask2.c

--- mpitask2.c
+++ mpitask2.c
@@ -4,15 +4,16 @@
 #include <math.h>
 
 double f(double x) {
-	return (1 + x * x*x);
+	return (2 + x );
 }
 
 int main(int argc, char** argv) {
 	int rank, size;
-	double a = 0.0, b = 10.0;
+	double a = 0.0, b = 5.0;
 	MPI_Init(&argc, &argv);
 	MPI_Comm_rank(MPI_COMM_WORLD, &rank);
 	MPI_Comm_size(MPI_COMM_WORLD, &size);
+	MPI_Status status;
 	int pro_num = size;
 	int per_num = 10000, k;
 	double w = (b - a) / (1.0 * pro_num * per_num);
@@ -22,6 +23,7 @@ int main(int argc, char** argv) {
 		part_num += f(x) * w;
 	}
 	if (0 != rank) {
+		MPI_Recv(rank, 1, MPI_DOUBLE, rank, rank, MPI_COMM_WORLD, &status);
 		MPI_Send(&part_num, 1, MPI_DOUBLE, 0, rank, MPI_COMM_WORLD);
 	}
 	else {
@@ -29,10 +31,15 @@ int main(int argc, char** argv) {
 		double result = part_num;
 		double temp_res;
 		for (j = 1; j < size; ++j) {
-			MPI_Recv(&temp_res, 1, MPI_DOUBLE, j, j, MPI_COMM_WORLD, MPI_STATUSES_IGNORE);
+			MPI_Send(j, 1, MPI_DOUBLE, 0, rank, MPI_COMM_WORLD);
+		}
+		for (j = 1; j < size; ++j) {
+			MPI_Probe(MPI_ANY_SOURCE, rank, MPI_COMM_WORLD, &status);
+			MPI_Get_count(&status, MPI_CHAR, &temp_res);
+			MPI_Recv(&temp_res, 1, MPI_DOUBLE, j, j, MPI_COMM_WORLD, &status);
 			result += temp_res;
 		}
-		printf("result is : %.f\n", result);
+		printf("result is : %.3f\n", result);
 	}
 	MPI_Finalize();
 	return 0;
